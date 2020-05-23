570c2cfdf9f7e27ab28a2f5af098bece9232156b
layzert <layzert@gmail.com>
2020-05-23 18:45:54 +0200

    fill db
    fill db & update dao

--- app/src/main/java/com/cleanup/todoc/database/TodocDatabase.java
+++ app/src/main/java/com/cleanup/todoc/database/TodocDatabase.java
@@ -1,4 +1,60 @@
 package com.cleanup.todoc.database;
 
-public class TodocDatabase {
+import android.arch.persistence.db.SupportSQLiteDatabase;
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.ContentValues;
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.cleanup.todoc.database.dao.ProjectDao;
+import com.cleanup.todoc.database.dao.TaskDao;
+import com.cleanup.todoc.model.Project;
+import com.cleanup.todoc.model.Task;
+
+@Database(entities = {Task.class, Project.class}, version = 1, exportSchema = false)
+public abstract class TodocDatabase extends RoomDatabase {
+
+    // --- SINGLETON ---
+    private static volatile TodocDatabase INSTANCE;
+
+    // --- DAO ---
+    public abstract TaskDao taskDao();
+    public abstract ProjectDao projectDao();
+
+    // --- INSTANCE ---
+    public static TodocDatabase getInstance(Context context) {
+        if (INSTANCE == null) {
+            synchronized (TodocDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            TodocDatabase.class, "MyDatabase.db")
+//                            .addCallback(prepopulateDatabase())
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    // ---
+
+//    private static Callback prepopulateDatabase(){
+//        return new Callback() {
+//
+//            @Override
+//            public void onCreate(@NonNull SupportSQLiteDatabase db) {
+//                super.onCreate(db);
+//
+//                ContentValues contentValues = new ContentValues();
+//                contentValues.put("id", 1);
+//                contentValues.put("username", "Philippe");
+//                contentValues.put("urlPicture", "https://oc-user.imgix.net/users/avatars/15175844164713_frame_523.jpg?auto=compress,format&q=80&h=100&dpr=2");
+//
+//                db.insert("User", OnConflictStrategy.IGNORE, contentValues);
+//            }
+//        };
+//    }
 }
--- app/src/main/java/com/cleanup/todoc/database/dao/TaskDao.java
+++ app/src/main/java/com/cleanup/todoc/database/dao/TaskDao.java
@@ -1,4 +1,27 @@
 package com.cleanup.todoc.database.dao;
 
-public class TaskDao {
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import com.cleanup.todoc.model.Task;
+
+import java.util.List;
+
+@Dao
+public interface TaskDao {
+
+    @Query("SELECT * FROM Task WHERE projectId = :projectId")
+    LiveData<List<Task>> getTask(long projectId);
+
+    @Insert
+    long insertTask(Task task);
+
+    @Update
+    int updateTask(Task task);
+
+    @Query("DELETE FROM Task WHERE id = :taskId")
+    int deleteTask(long taskId);
 }
