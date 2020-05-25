39d75a1d192c89ca8e4a91d6826c09d1bca23a02
layzert <layzert@gmail.com>
2020-05-25 19:32:26 +0200

    Repo creation
    New ProjectDataRepository and TaskDataRepository

---
+++ app/src/main/java/com/cleanup/todoc/repositories/TaskDataRepository.java
@@ -0,0 +1,30 @@
+package com.cleanup.todoc.repositories;
+
+import android.arch.lifecycle.LiveData;
+
+import com.cleanup.todoc.database.dao.TaskDao;
+import com.cleanup.todoc.model.Task;
+
+import java.util.List;
+
+public class TaskDataRepository {
+
+    private final TaskDao mTaskDao;
+
+    public TaskDataRepository(TaskDao taskDao) {
+        mTaskDao = taskDao;
+    }
+
+    public LiveData<List<Task>> getTasks(long projectId) {
+        return this.mTaskDao.getTask(projectId);
+    }
+    // --- CREATE ---
+
+    public void createTask(Task task){ mTaskDao.insertTask(task); }
+
+    // --- DELETE ---
+    public void deleteTask(long taskId){ mTaskDao.deleteTask(taskId); }
+
+    // --- UPDATE ---
+    public void updateTask(Task task){ mTaskDao.updateTask(task); }
+}
